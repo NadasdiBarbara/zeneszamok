d865f769b9f763217d404f44aeac2a0e7991daee
NadasdiBarara <nadasdibarbara15@gmail.com>
2022-01-07 15:07:15 +0100

    first commit

---
+++ database/factories/SongFactory.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class SongFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'title' => $this->faker->words(2, true),
+            'singer' => $this->faker->name(),
+            'length' => rand(60,2000),
+        ];
+    }
+}
